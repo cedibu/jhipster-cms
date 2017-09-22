cd94c04f29d6a4ee9348f070941121d4a46d7ad3
Cedric <cedric.bulloni@gmail.com>
2017-09-22 16:00:44 +0200

    Initial commit

---
+++ src/main/java/org/jhipster/domain/Article.java
@@ -0,0 +1,92 @@
+package org.jhipster.domain;
+
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Article.
+ */
+@Entity
+@Table(name = "article")
+public class Article implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    // jhipster-needle-entity-add-field - Jhipster will add fields here, do not remove
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Article title(String title) {
+        this.title = title;
+        return this;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Article content(String content) {
+        this.content = content;
+        return this;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+    // jhipster-needle-entity-add-getters-setters - Jhipster will add getters and setters here, do not remove
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Article article = (Article) o;
+        if (article.getId() == null || getId() == null) {
+            return false;
+        }
+        return Objects.equals(getId(), article.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+            "id=" + getId() +
+            ", title='" + getTitle() + "'" +
+            ", content='" + getContent() + "'" +
+            "}";
+    }
+}
---
+++ src/main/java/org/jhipster/repository/ArticleRepository.java
@@ -0,0 +1,16 @@
+package org.jhipster.repository;
+
+import org.jhipster.domain.Article;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.*;
+
+
+/**
+ * Spring Data JPA repository for the Article entity.
+ */
+@SuppressWarnings("unused")
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
